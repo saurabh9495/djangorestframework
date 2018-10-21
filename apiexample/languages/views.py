ce73e39577715bdc60fe708884b9bfdb069f8dc4
saurabh9495 <saurabhkumar4829@gmail.com>
2018-10-22 01:19:10 +0530

    final Commit

--- apiexample/languages/views.py
+++ apiexample/languages/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from .models import Language, UpcomingProject, Current
 from .serializers import LanguageSerializer, UpcomingProjectSerializer, CurrentSerializer
 
 class LanguageView(viewsets.ModelViewSet):
     queryset = Language.objects.all()
     serializer_class = LanguageSerializer
+    # permission_classes = (permissions.IsAuthenticated,)
 
 class UpcomingProjectView(viewsets.ModelViewSet):
     queryset = UpcomingProject.objects.all()
     serializer_class = UpcomingProjectSerializer
+    # permission_classes = (permissions.IsAuthenticated,)
 
 class CurrentView(viewsets.ModelViewSet):
     queryset = Current.objects.all()
     serializer_class = CurrentSerializer
+    # permission_classes = (permissions.IsAuthenticated,)
